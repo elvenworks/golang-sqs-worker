5b140ac9e3412884a405737576d87e1bb70e9d24
yuriadams <yuriadams@gmail.com>
2021-04-13 16:14:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# go-sqs-poller
+
+GoLang SQS Queue Poller using the AWS SDK
+
+This is based off of [golang-sqs-worker-example](https://github.com/nabeken/golang-sqs-worker-example) but it uses the [official AWS golang SDK](https://github.com/aws/aws-sdk-go).
+
+Check out the [cmd/example-worker/main.go](cmd/example-worker/main.go) for an example of how to use the worker.
---
+++ cmd/example-worker/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/elvenworks/go-sqs-poller/v3/worker"
+)
+
+func main() {
+	awsConfig := &aws.Config{
+		Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY"), os.Getenv("AWS_SECRET_KEY"), ""),
+		Region:      aws.String(os.Getenv("AWS_REGION")),
+	}
+	sqsClient := worker.CreateSqsClient(awsConfig)
+	workerConfig := &worker.Config{
+		QueueName:          "my-sqs-queue",
+		MaxNumberOfMessage: 10,
+		WaitTimeSecond:     5,
+	}
+	eventWorker := worker.New(sqsClient, workerConfig)
+	ctx := context.Background()
+
+	// start the worker
+	eventWorker.Start(ctx, worker.HandlerFunc(func(msg *sqs.Message) error {
+		fmt.Println(aws.StringValue(msg.Body))
+		return nil
+	}))
+}
